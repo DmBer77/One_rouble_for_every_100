921a4cb5ff70e2dce1ede81b0a281af3f1aafd6b
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-05-17 18:57:38 +0300

    add changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(5);
+        int account = 1_800;
+        int addSumm = 1_350;
+        int bonus;
+
+        if (addSumm >= 1_000) {
+            bonus = addSumm / 100;
+            account = account + addSumm + bonus;
+        } else {
+            account = account + addSumm;
+        }
+
+        System.out.println("Сумма на счет составляет: " + account);
     }
 }
